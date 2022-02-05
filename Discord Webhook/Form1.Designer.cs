fd968b9161e4fb664e45d49478005602b734df56
SUPER-PROJECTS-1 <99094981+SUPER-PROJECTS-1@users.noreply.github.com>
2022-02-05 20:29:28 +0000

    Add files via upload

---
+++ Discord Webhook/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Discord_Webhook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.siticonePanel1 = new Siticone.UI.WinForms.SiticonePanel();
+            this.siticoneControlBox1 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.singmgs = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.singuser = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.singweb = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.siticonePanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // siticonePanel1
+            // 
+            this.siticonePanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.siticonePanel1.Controls.Add(this.siticoneControlBox1);
+            this.siticonePanel1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.siticonePanel1.Location = new System.Drawing.Point(-5, -63);
+            this.siticonePanel1.Name = "siticonePanel1";
+            this.siticonePanel1.ShadowDecoration.Parent = this.siticonePanel1;
+            this.siticonePanel1.Size = new System.Drawing.Size(548, 100);
+            this.siticonePanel1.TabIndex = 2;
+            // 
+            // siticoneControlBox1
+            // 
+            this.siticoneControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.siticoneControlBox1.HoveredState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.Location = new System.Drawing.Point(438, 68);
+            this.siticoneControlBox1.Name = "siticoneControlBox1";
+            this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox1.TabIndex = 3;
+            // 
+            // singmgs
+            // 
+            this.singmgs.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(84)))), ((int)(((byte)(103)))));
+            this.singmgs.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.singmgs.DefaultText = "";
+            this.singmgs.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.singmgs.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.singmgs.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.singmgs.DisabledState.Parent = this.singmgs;
+            this.singmgs.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.singmgs.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.singmgs.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.singmgs.FocusedState.Parent = this.singmgs;
+            this.singmgs.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.singmgs.HoveredState.Parent = this.singmgs;
+            this.singmgs.Location = new System.Drawing.Point(59, 184);
+            this.singmgs.Name = "singmgs";
+            this.singmgs.PasswordChar = '\0';
+            this.singmgs.PlaceholderText = "";
+            this.singmgs.SelectedText = "";
+            this.singmgs.ShadowDecoration.Parent = this.singmgs;
+            this.singmgs.Size = new System.Drawing.Size(369, 30);
+            this.singmgs.TabIndex = 108;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(56, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 107;
+            this.label5.Text = "Message:";
+            // 
+            // singuser
+            // 
+            this.singuser.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(84)))), ((int)(((byte)(103)))));
+            this.singuser.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.singuser.DefaultText = "";
+            this.singuser.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.singuser.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.singuser.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.singuser.DisabledState.Parent = this.singuser;
+            this.singuser.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.singuser.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.singuser.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.singuser.FocusedState.Parent = this.singuser;
+            this.singuser.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.singuser.HoveredState.Parent = this.singuser;
+            this.singuser.Location = new System.Drawing.Point(59, 125);
+            this.singuser.Name = "singuser";
+            this.singuser.PasswordChar = '\0';
+            this.singuser.PlaceholderText = "";
+            this.singuser.SelectedText = "";
+            this.singuser.ShadowDecoration.Parent = this.singuser;
+            this.singuser.Size = new System.Drawing.Size(369, 30);
+            this.singuser.TabIndex = 106;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(56, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 105;
+            this.label1.Text = "Nickname:";
+            // 
+            // singweb
+            // 
+            this.singweb.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(84)))), ((int)(((byte)(103)))));
+            this.singweb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.singweb.DefaultText = "";
+            this.singweb.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.singweb.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.singweb.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.singweb.DisabledState.Parent = this.singweb;
+            this.singweb.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.singweb.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.singweb.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.singweb.FocusedState.Parent = this.singweb;
+            this.singweb.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.singweb.HoveredState.Parent = this.singweb;
+            this.singweb.Location = new System.Drawing.Point(59, 67);
+            this.singweb.Name = "singweb";
+            this.singweb.PasswordChar = '\0';
+            this.singweb.PlaceholderText = "";
+            this.singweb.SelectedText = "";
+            this.singweb.ShadowDecoration.Parent = this.singweb;
+            this.singweb.Size = new System.Drawing.Size(369, 30);
+            this.singweb.TabIndex = 104;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(56, 51);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.TabIndex = 103;
+            this.label8.Text = "Webhook:";
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.siticoneButton1.FillColor = System.Drawing.Color.DodgerBlue;
+            this.siticoneButton1.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(59, 222);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(369, 23);
+            this.siticoneButton1.TabIndex = 109;
+            this.siticoneButton1.Text = "Send Message";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this.siticonePanel1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(484, 255);
+            this.Controls.Add(this.siticoneButton1);
+            this.Controls.Add(this.singmgs);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.singuser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.singweb);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.siticonePanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.siticonePanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Siticone.UI.WinForms.SiticonePanel siticonePanel1;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox1;
+        private Siticone.UI.WinForms.SiticoneTextBox singmgs;
+        private System.Windows.Forms.Label label5;
+        private Siticone.UI.WinForms.SiticoneTextBox singuser;
+        private System.Windows.Forms.Label label1;
+        private Siticone.UI.WinForms.SiticoneTextBox singweb;
+        private System.Windows.Forms.Label label8;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+    }
+}
+
---
+++ Discord Webhook/Form1.cs
@@ -0,0 +1,41 @@
+﻿using ya;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Webhook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void sendDiscordWeb(string URL, string msg, string user)
+        {
+            Http.Post(URL, new System.Collections.Specialized.NameValueCollection()
+           {
+               {
+                   "username",
+                    user
+               },
+               {
+                   "content",
+                   msg
+               }
+           });
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            sendDiscordWeb(singweb.Text, singmgs.Text, singuser.Text);
+        }
+    }
+}
